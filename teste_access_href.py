8ad5c2cab6b73a1de64e13446b7b99690e82c3a8
LivSith <liviasithmendes@gmail.com>
2020-11-29 16:40:17 -0300

    Test changing page...

--- teste_access_href.py
+++ teste_access_href.py
@@ -1,19 +1,55 @@
+import os
 from bs4 import BeautifulSoup
-import re
 import requests
+import lxml
 
-pages = set()
+import pandas as pd
 
-def get_links(page_url):
-    pattern = re.compile("^(/)")
-    html = requests.get(page_url).text
-    soup = BeautifulSoup(html, "html.parser")
-    for link in soup.find_all("a", href=pattern):
-        if "href" in link.attrs:
-            if link.attrs["href"] not in pages:
-                new_page = link.attrs["href"]
-                print(new_page)
-                pages.add(new_page)
-                get_links(new_page)
+
+search_url = 'https://github.com/frontpressorg/frontpress'
+base_url = 'https://github.com/'
+repo_url = 'frontpressorg/frontpress'
+
+
+def export_table_and_print(data):
+    #cria tabela de resultado    
+    table = pd.DataFrame(data, columns=['Files', 'URLs'])
+    table.index = table.index + 1
+    # cria pasta resultado
+    dir = './resultados'
+    os.mkdir(dir)
+    # escreve resultado
+    repo_name = (repo_url.lower().replace('/', '_'))
+    file_name = "resultados/"+repo_name+"_scraping.txt"
+    table.to_csv(file_name, sep=',', encoding='utf-8', index=False)
+    print('Scraping done. Here are the results:')
+    print(table)
+
+
+    
+def get_repo_data(file):
+    name_file = file.find('a', class_='js-navigation-open link-gray-dark').text
+    url_file = file.find('a')['href'] if name_file else ''
         
-get_links("https://www.climatempo.com.br/")
+    data['Files'].append(name_file if name_file else '')
+    data['URLs'].append(url_file if url_file else '')
+    
+# HTTP GET requests
+page = requests.get(search_url)
+
+if page.status_code == requests.codes.ok:
+    bs = BeautifulSoup(page.text, 'lxml')
+    list_all_files = bs.findAll("div", class_="Box-row Box-row--focus-gray py-2 d-flex position-relative js-navigation-item")
+    
+    data = {
+        'Files': [],
+        'URLs': [],
+    }
+        
+    for file in list_all_files:
+        get_repo_data(file)
+    
+    export_table_and_print(data)
+  
+parse_page(url)
+#print(data)
