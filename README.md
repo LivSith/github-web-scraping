006f36da1aed2b98f6c104e07ec4a5d490df6e4d
LivSith <liviasithmendes@gmail.com>
2020-11-30 11:27:21 -0300

    update README

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # github-web-scraping
 Aplicação que utiliza web scraping para buscar informações de projetos públicos do Github.
+
+
+## Como rodar o projeto
+
+
+### 1. Clone este repositório:
+
+No terminal digite:
+
+> $ git clone https://github.com/LivSith/github-web-scraping.git
+
+
+
+### 2. Passe a lista de repositórios
+Crie um arquivo repositories.txt, na pasta raiz.
+
+Exemplo, como os repositórios têm de ser escrito:
+``` 
+frontpressorg/frontpress
+SambitAcharya/Mini-Projects
+```
+
+### 3. Verique onde vc está
+No terminal, vá até a pasta do projeto. Provavelmente você verá escrito no terminal:
+> home/seu_usuario/pasta_do_projeto$
+
+### 4. Aperta o play
+Vamos rodar a aplicação, no terminal, então digite:
+>$ python3 main.py
+
--- teste_access_href.py
+++ teste_access_href.py
@@ -1,5 +1,8 @@
 import os
+import collections, functools, operator
 from bs4 import BeautifulSoup
+
+from decimal import Decimal
 import requests
 import lxml
 
@@ -12,16 +15,15 @@ repo_url = 'frontpressorg/frontpress'
 
 
 data = {
-        'Files': [],
-        'URLs': [],
-        'Type_of_file': [],
+        'Extension': [],
         'Linhas': [],
         'Bytes': [],
     }
 
+
 def export_table_and_print(data):
     #cria tabela de resultado    
-    table = pd.DataFrame(data, columns=['Files', 'URLs', 'Type_of_file', 'Linhas', 'Bytes'])
+    table = pd.DataFrame(data, columns=['Extension', 'Linhas', 'Bytes',])
     table.index = table.index + 1
     # cria pasta resultado
     dir = './resultados'
@@ -40,12 +42,16 @@ def get_repo_data(file):
     url_file = file.find('a')['href'] if name_file else ''
     type_of_file = file.find('svg')['aria-label'] if name_file else ''
         
-    data['Files'].append(name_file if name_file else '')
-    data['URLs'].append(url_file if url_file else '')
-    data['Type_of_file'].append(type_of_file if type_of_file else '')
+    #data['Files'].append(name_file if name_file else '')
+    #data['Type_of_file'].append(type_of_file if type_of_file else '')
     
     new_repo_url = base_url+url_file
     new_page=requests.get(new_repo_url)
+    
+    extension_type = name_file.split('.')
+    extension = extension_type[-1]
+    
+    
     
     if new_page.status_code == requests.codes.ok:
         bs_repo = BeautifulSoup(new_page.text, 'lxml')
@@ -55,15 +61,25 @@ def get_repo_data(file):
             
             dados_limpo = find_dados.lstrip()
             dados_list = dados_limpo.split()
-            lines = float(dados_list[0])
-            size = float(dados_list[-2])
-            print('linhas: ', lines, "| tamanho: ", size)
-            data['Linhas'].append(lines if lines else '')
-            data['Bytes'].append(size if size else '')
-        
+            lines = Decimal(dados_list[0])
+            size = Decimal(dados_list[-2])
+            
+            data['Extension'].append(extension)
+            data['Linhas'].append(lines)
+            data['Bytes'].append(size)
+            
+            
+            #if extension not in data['Extension']:
+                #data['Extension'].append(extension)
+                #data['Linhas'].append(lines)
+                #data['Bytes'].append(size)
+
         else:
             data['Linhas'].append('')
             data['Bytes'].append('')
+            data['Extension'].append('')
+            
+        
 #def parse_page(next_url):
     
 # HTTP GET requests
@@ -75,12 +91,14 @@ if page.status_code == requests.codes.ok:
     list_all_files = bs.findAll("div", class_="Box-row Box-row--focus-gray py-2 d-flex position-relative js-navigation-item")
     
     
-        
+total_lines=0       
 for file in list_all_files:
     get_repo_data(file)
 
-    
+
 export_table_and_print(data)
-  
+
+counter = collections.Counter() 
+
 #parse_page(url)
 #print(data)
