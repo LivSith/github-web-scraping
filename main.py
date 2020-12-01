aa9fc0be96aeabbf9a2c8eb80b046296f3169bc0
LivSith <liviasithmendes@gmail.com>
2020-11-30 22:44:28 -0300

    create main.py file...

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import os
-from bs4 import BeautifulSoup
-
-import requests
 import lxml
-
+import requests
 import pandas as pd
-
-
-search_url = 'https://github.com/frontpressorg/frontpress'
-base_url = 'https://github.com/'
-repo_url = 'frontpressorg/frontpress'
+from bs4 import BeautifulSoup
 
 
 data = {
@@ -25,21 +18,23 @@ def export_table_and_print(data):
     table.index = table.index + 1
     # cria pasta resultado
     dir = './resultados'
-    os.mkdir(dir)
+    if not os.path.exists(dir):
+        os.mkdir(dir)
     # escreve resultado
-    repo_name = (repo_url.lower().replace('/', '_'))
+    repo_name = ''.join(repo_url.split('/')[3:])
+    print  (repo_name)
     file_name = "resultados/"+repo_name+"_scraping.txt"
     table.to_csv(file_name, sep=',', encoding='utf-8', index=False)
     print('Scraping done. Here are the results:')
     print(table)
 
 
-    
+# Scraping de dados
 def get_repo_data(file):
-    
     name_file = file.find('a', class_='js-navigation-open link-gray-dark').text
     url_file = file.find('a')['href'] if name_file else ''
     type_of_file = file.find('svg')['aria-label'] if name_file else ''
+        
     #data['Files'].append(name_file if name_file else '')
     #data['Type_of_file'].append(type_of_file if type_of_file else '')
     
@@ -49,8 +44,6 @@ def get_repo_data(file):
     extension_type = name_file.split('.')
     extension = extension_type[-1]
     
-    
-    
     if new_page.status_code == requests.codes.ok:
         bs_repo = BeautifulSoup(new_page.text, 'lxml')
         
@@ -59,8 +52,8 @@ def get_repo_data(file):
             
             dados_limpo = find_dados.lstrip()
             dados_list = dados_limpo.split()
-            lines = float(dados_list[0])
-            size = float(dados_list[-2])
+            lines = dados_list[0]
+            size = dados_list[-2]
             
             data['Extension'].append(extension)
             data['Linhas'].append(lines)
@@ -76,36 +69,22 @@ def get_repo_data(file):
             data['Linhas'].append('')
             data['Bytes'].append('')
             data['Extension'].append('')
-            
-            url_file = file.find('a')['href'] if name_file else ''
-            type_of_file = file.find('svg')['aria-label'] if name_file else ''
-            
-            in_new_repo_url = base_url+url_file
-            in_new_page=requests.get(in_new_repo_url)
 
+
+# Ler arquivo .txt
+repositorios = open("repositories.txt", "r")
+for repo in repositorios:
+    base_url = 'https://github.com/'
+    repo_url = base_url+repo.rstrip()
     
-    new_repo_url = base_url+url_file
-    new_page=requests.get(new_repo_url)
-            
+    page = requests.get(repo_url)
+    
+    if page.status_code == requests.codes.ok:
+        bs = BeautifulSoup(page.text, 'lxml')
         
-#def parse_page(next_url):
+        list_all_files = bs.findAll("div", class_="Box-row Box-row--focus-gray py-2 d-flex position-relative js-navigation-item")
     
-# HTTP GET requests
-print(search_url)
-page = requests.get(search_url)
-print(page)
-if page.status_code == requests.codes.ok:
-    bs = BeautifulSoup(page.text, 'lxml')
-        
-    list_all_files = bs.findAll("div", class_="Box-row Box-row--focus-gray py-2 d-flex position-relative js-navigation-item")
-    
-    
-total_lines=0       
-for file in list_all_files:
-    get_repo_data(file)
-
-
-export_table_and_print(data)
-
-#parse_page(url)
-#print(data)
+        for file in list_all_files:
+            get_repo_data(file)
+            
+        export_table_and_print(data)
